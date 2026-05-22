4ad873e42db5f4e387a8236094270694035c62bc
Ary3126 <patelary9054@gmail.com>
2026-05-22 15:35:09 +0530

    Delete day-3/script1.js

--- day-3/script1.js
+++
@@ -1,9 +0,0 @@
-let btn=document.createElement("button");
-btn.innerText="Click me";
-btn.style.color="white";
-btn.style.backgroundColor="blue";
-btn.style.padding="10px 20px";
-btn.style.border="none";
-btn.style.borderRadius="5px";
-btn.style.cursor="pointer";
-document.querySelector("body").prepend(btn);
